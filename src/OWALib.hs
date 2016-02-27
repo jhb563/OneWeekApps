49c7357c0893f5c9dbd1b29419bbc5dc6656a292
jhb563 <jhbowen047@gmail.com>
2016-02-27 11:01:37 -0800

    Updated runOWA

--- src/OWALib.hs
+++ src/OWALib.hs
@@ -6,7 +6,34 @@ License     : MIT
 Maintainer  : jhbowen047@gmail.com
 -}
 
-module OWALib where
+module OWALib (
+  runOWA
+) where
+
+import OWAColorObjc
+import OWAColorParser
+import OWAFileSearch
+import OWAObjcPrint
 
 runOWA :: FilePath -> IO ()
-runOWA filePath = print "Running OWA!"
+runOWA filePath = do
+  maybeAppDirectory <- findAppDirectory filePath
+  case maybeAppDirectory of 
+    Nothing -> print "Could not find app directory!"
+    Just appDirectory -> do
+      colorFiles <- findColorsFiles appDirectory
+      listOfColorLists <- mapM parseColorsFromFile colorFiles
+      let colors = concat listOfColorLists
+      let colorHeaderFileStructure = objcHeaderFromColors colorCategoryName colors
+      let colorMFileStructure = objcImplementationFromColors colorCategoryName colors
+      printStructureToFile colorHeaderFileStructure (appDirectory ++ colorHeaderFileExtension)
+      printStructureToFile colorMFileStructure (appDirectory ++ colorImplementationFileExtension)
+
+colorCategoryName :: String
+colorCategoryName = "MyAppColors"
+
+colorHeaderFileExtension :: FilePath
+colorHeaderFileExtension = "/UIColor+MyAppColors.h"
+
+colorImplementationFileExtension :: FilePath
+colorImplementationFileExtension = "/UIColor+MyAppColors.m"
