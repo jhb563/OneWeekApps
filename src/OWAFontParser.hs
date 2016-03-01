c3322a732ec7af304b0120bb18335d7d6d6cf7da
jhb563 <jhbowen047@gmail.com>
2016-03-01 09:02:30 -0800

    Most of FontParser written.

--- src/OWAFontParser.hs
+++ src/OWAFontParser.hs
@@ -13,7 +13,14 @@ module OWAFontParser (
 import Data.Maybe
 import OWAFont
 import ParseUtil
+import qualified Data.Map.Strict as Map
 import Text.Parsec.Error
+
+type FontAttr = String
+data FontVal = FamilyVal String |
+  SizeVal Float |
+  StyleAttrs [String]
+type FontAttrMap = Map.Map FontAttr FontVal
 
 ---------------------------------------------------------------------------
 --------------------ENTRY METHODS------------------------------------------
@@ -30,3 +37,62 @@ parseFontsFromFile fPath = do
 parseFontContents :: String -> Either ParseError [Maybe OWAFont]
 parseFontContents contents = Right []
 
+-------------------------------------------------------------------------------
+-----------------------------------PARSERS-------------------------------------
+-------------------------------------------------------------------------------
+
+fontParser :: GenParser Char st (Maybe OWAColor)
+fontParser = do
+  spaces
+  name <- nameParserWithKeyword fontKeyword
+  attrs <- many1 fontAttrLine
+  let attrMap = Map.fromList attrs
+  return (fontFromNameAndAttrMap name attrMap)
+
+fontAttrLine :: GenParser Char st (FontAttr, FontVal)
+fontAttrLine = do
+  string "\t" <|> string "  "
+  choice fontAttrParsers
+
+fontAttrParsers :: [GenParser Char st (FontAttr, FontVal)]
+fontAttrParsers = [fontFamilyParser, fontSizeParser, fontStylesParser]
+
+fontFamilyParser :: GenParser Char st (FontAttr, FontVal)
+fontFamilyParser = do
+  string fontFamilyKeyword
+  char ' '
+  familyName <- many (alphaNum <|> char '-')
+  return (fontFamilyKeyword, FamilyVal familyName)
+
+fontSizeParser :: GenParser Char st (FontAttr, FontVal)
+fontSizeParser = do
+  (_, floatVal) <- floatAttributeParser fontSizeKeyword
+  return (fontSizeKeyword, SizeVal floatVal)
+
+fontStylesParser :: GenParser Char st (FontAttr, FontVal)
+fontStylesParser = do
+  string fontStylesKeyword
+  char ' '
+  -- TODO: Comma Separated List of style vals
+
+-------------------------------------------------------------------------------
+--------------CONSTRUCTING FONTS-----------------------------------------------
+-------------------------------------------------------------------------------
+
+fontFromNameAndAttrMap :: String -> FontAttrMap -> Maybe OWAFont
+
+-------------------------------------------------------------------------------
+-------------------FONT KEYWORDS-----------------------------------------------
+-------------------------------------------------------------------------------
+
+fontKeyword :: String
+fontKeyword = "Font"
+
+fontFamilyKeyword :: String
+fontFamilyKeyword = "FontFamily"
+
+fontSizeKeyword :: String
+fontSizeKeyword = "Size"
+
+fontStylesKeyword :: String
+fontStylesKeyword = "Styles"
