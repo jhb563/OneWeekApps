713c6b2d2ec23cf3595d8e446db1c8e8048b46cb
jhb563 <jhbowen047@gmail.com>
2016-03-14 14:59:16 -0700

    Added haddock.

--- src/dataTypes/OWAParseError.hs
+++ src/dataTypes/OWAParseError.hs
@@ -1,7 +1,18 @@
+{-|
+Module      : OWAParseError
+Description : Module encapsulating possible parse errors in OWA.
+Copyright   : (c) James Bowen, 2016
+License     : MIT
+Maintainer  : jhbowen047@gmail.com
+-}
+
 module OWAParseError where
 
 import Text.Parsec.Error
 
+-- | OWAParseErrors can take two forms. They are either a wrapper
+-- around an error returned by Parsec, or they are a description of
+-- an insufficiently attributed item. 
 data OWAParseError = ParsecError ParseError |
   ObjectError {
     itemName :: String,
