ab469442d9ba4029030ad8d8dbd11c6124dc943b
jhb563 <jhbowen047@gmail.com>
2016-05-16 22:10:47 -0700

    Test harness set up for printing.

--- tests/Version021Tests/CustomViewTests/CustomViewTests.hs
+++ tests/Version021Tests/CustomViewTests/CustomViewTests.hs
@@ -2,20 +2,36 @@
 -- parseViewFromFile :: FilePath -> IO Either [OWAParseError] OWAView
 -- which parses a view from a .view file when given a filepath, looking
 -- at the specific case of parsing custom views.
+-- 
+-- It will also test the methods:
+-- objcHeaderFromView :: OWAAppInfo -> OWAView -> ObjcFile
+-- objcImplementationFromView :: OWAAppInfo -> OWAView -> ObjcFile
+-- from OWAViewObjc follwed by:
+-- printStructureToFile :: ObjcFile -> Doc
+-- in OWAObjcPrint, again testing the custom views case
 
 module CustomViewTests (
   runCustomViewTests
 ) where
 
+import OWAAppInfo
+import OWAObjcAbSyn
+import OWAObjcPrint
 import OWAViewParser
 import TestUtil
 import TestViews
 import Test.Hspec
 
 runCustomViewTests :: FilePath -> IO ()
-runCustomViewTests currentDirectory = hspec $ do
+runCustomViewTests currentDirectory = do
   let parseDirectory = currentDirectory ++ parseDirectoryExtension
-  viewParseTests parseDirectory
+  let outputDirectory = currentDirectory ++ outputDirectoryExtension
+  hspec $
+    beforeAll_ (removeDiffFiles outputDirectory) $
+    beforeAll_ (createResultsFiles outputDirectory resultsFiles testFileStructures)
+    . afterAll_ (removeResultsFiles outputDirectory resultsFiles) $ do
+      viewParseTests parseDirectory
+      viewPrintTests outputDirectory
 
 viewParseTests :: FilePath -> Spec
 viewParseTests parseDirectory = do
@@ -35,6 +51,54 @@ viewParseTests parseDirectory = do
       it "Should match the test view" $
         parseViewFromFile testFile3 `shouldReturnRights` twoDifferentCustomTest
 
+viewPrintTests :: FilePath -> Spec
+viewPrintTests outputDirectory = describe "Print File Structure for views with custom views" $ do
+  context "When there is a single custom view" $ do
+    it "The printed header should match" $
+      (outputDirectory ++ basicHeaderResultFile) `filesShouldMatch` (outputDirectory ++ basicHeaderTestFile)
+
+    it "The printed implementation should match" $ 
+      (outputDirectory ++ basicMResultFile) `filesShouldMatch` (outputDirectory ++ basicMTestFile)
+
+  context "When there are two custom subvies of the same type" $ do
+    it "The printed header should match" $
+      (outputDirectory ++ twoSameHeaderResultFile) `filesShouldMatch` (outputDirectory ++ twoSameHeaderTestFile)
+
+    it "The printed implementation should match" $
+      (outputDirectory ++ twoSameMResultFile) `filesShouldMatch` (outputDirectory ++ twoSameMTestFile)
+
+  context "When there are two custom views of different types" $ do
+    it "The printed header should match" $
+      (outputDirectory ++ twoDifferentHeaderResultFile) `filesShouldMatch` (outputDirectory ++ twoDifferentHeaderTestFile)
+      
+    it "The printed implementation should match" $
+      (outputDirectory ++ twoDifferentMResultFile) `filesShouldMatch` (outputDirectory ++ twoDifferentMTestFile)
+
+sampleAppInfo :: OWAAppInfo
+sampleAppInfo = OWAAppInfo {
+  appName = "MySampleApp",
+  appPrefix = "MSA",
+  authorName = "James Bowen",
+  dateCreatedString = "4/30/2016",
+  companyName = Just "One Week Apps"
+}
+
+testFileStructures :: [ObjcFile]
+testFileStructures = [objcHeaderFromView sampleAppInfo basicCustomTest,
+  objcImplementationFromView sampleAppInfo basicCustomTest,
+  objcHeaderFromView sampleAppInfo twoSameCustomTest,
+  objcImplementationFromView sampleAppInfo twoSameCustomTest,
+  objcHeaderFromView sampleAppInfo twoDifferentCustomTest,
+  objcImplementationFromView sampleAppInfo twoDifferentCustomTest]
+
+resultsFiles :: [String]
+resultsFiles = [basicHeaderResultFile,
+  basicMResultFile,
+  twoSameHeaderResultFile,
+  twoSameMResultFile,
+  twoDifferentHeaderResultFile,
+  twoDifferentMResultFile]
+
 parseDirectoryExtension :: String
 parseDirectoryExtension = "/tests/Version021Tests/CustomViewTests/ParseFiles"
 
@@ -46,3 +110,42 @@ twoSameParseExtension = "/twoSameCustomTest.view"
 
 twoDifferentParseExtension :: String
 twoDifferentParseExtension = "/twoDifferentCustomTest.view"
+
+outputDirectoryExtension :: String
+outputDirectoryExtension = "/tests/Version021Tests/CustomViewTests/OutputFiles"
+
+basicHeaderResultFile :: String
+basicHeaderResultFile = "/IGACustomTest1.h"
+
+basicMResultFile :: String
+basicMResultFile = "/IGACustomTest1.m"
+
+twoSameHeaderResultFile :: String
+twoSameHeaderResultFile = "/IGACustomTest2.h"
+
+twoSameMResultFile :: String
+twoSameMResultFile = "/IGACustomTest2.m"
+
+twoDifferentHeaderResultFile :: String
+twoDifferentHeaderResultFile = "/IGACustomTest3.h"
+
+twoDifferentMResultFile :: String
+twoDifferentMResultFile = "/IGACustomTest3.m"
+
+basicHeaderTestFile :: String
+basicHeaderTestFile = "/IGACustomTest1.h.test"
+
+basicMTestFile :: String
+basicMTestFile = "/IGACustomTest1.m.test"
+
+twoSameHeaderTestFile :: String
+twoSameHeaderTestFile = "/IGACustomTest2.h.test"
+
+twoSameMTestFile :: String
+twoSameMTestFile = "/IGACustomTest2.m.test"
+
+twoDifferentHeaderTestFile :: String
+twoDifferentHeaderTestFile = "/IGACustomTest3.h.test"
+
+twoDifferentMTestFile :: String
+twoDifferentMTestFile = "/IGACustomTest3.m.test"
