c15a8a26e853e5d00b22575e44e612ee7f8955e6
jhb563 <jhbowen047@gmail.com>
2017-04-28 12:15:00 -0700

    Implemented Xcode functions, tests pass.

--- owa-objc/src/Objc/XCode.hs
+++ owa-objc/src/Objc/XCode.hs
@@ -16,15 +16,105 @@ module Objc.XCode
 
 import Model.OWAAppInfo
 import Objc.AbSyn
+import Objc.Utils
 
 initialVCHeader :: OWAAppInfo -> ObjcFile
-initialVCHeader _ = ObjcFile []
+initialVCHeader appInfo = ObjcFile 
+  [ topCommentSection "ViewController.h" appInfo
+  , uiKitImportsSection 
+  , interfaceSection ]
+  where
+    interfaceSection = InterfaceSection
+      "ViewController"
+      (Just "UIViewController")
+      Nothing
+      []
+      []
+      []
 
 initialVCImplementation :: OWAAppInfo -> ObjcFile
-initialVCImplementation _ = ObjcFile []
+initialVCImplementation appInfo = ObjcFile 
+  [ topCommentSection "ViewController.m" appInfo
+  , ImportsSection [FileImport "ViewController.h"] 
+  , ImplementationSection "ViewController" Nothing [lifecycleSection] ]
+  where
+    lifecycleSection = MethodImplementationListSection 
+      (Just "Lifecycle") 
+      [viewDidLoadMethod, updateViewConstraintsMethod]
+    viewDidLoadMethod = ObjcMethod False "viewDidLoad" (SimpleType "void") [] 
+      [ ExpressionStatement $ MethodCall (Var "super") (LibMethod "viewDidLoad" []) []
+      , ExpressionStatement $ MethodCall 
+        (PropertyCall SelfExpr "view")
+        (LibMethod "setNeedsUpdateConstraints" []) [] ]
+    updateViewConstraintsMethod = ObjcMethod 
+      False "updateViewConstraints" (SimpleType "void") [] 
+      [ExpressionStatement $ MethodCall (Var "super") (LibMethod "updateViewConstraints" []) []]
 
 appDelegateHeader :: OWAAppInfo -> ObjcFile
-appDelegateHeader _ = ObjcFile []
+appDelegateHeader appInfo = ObjcFile 
+  [ topCommentSection "AppDelegate.h" appInfo
+  , uiKitImportsSection 
+  , interfaceSection ]
+  where
+    interfaceSection = InterfaceSection 
+      "AppDelegate" 
+      (Just "UIResponder") 
+      Nothing
+      ["UIApplicationDelegate"]
+      [windowProperty]
+      []
+    windowProperty = ObjcProperty (PointerType "UIWindow") ["strong", "nonatomic"] "window"
 
 appDelegateImplementation :: OWAAppInfo -> ObjcFile
-appDelegateImplementation _ = ObjcFile []
+appDelegateImplementation appInfo = ObjcFile 
+  [ topCommentSection "AppDelegate.m" appInfo
+  , ImportsSection [FileImport "AppDelegate.h", FileImport "ViewController.h"] 
+  , ImplementationSection "AppDelegate" Nothing [appDelegateImplSection] ]
+  where
+    appDelegateImplSection = MethodImplementationListSection Nothing
+      ( didLaunchFunction :
+        map delegateFunction 
+          [ "WillResignActive"
+          , "DidEnterBackground"
+          , "WillEnterForeground"
+          , "DidBecomeActive"
+          , "WillTerminate" 
+          ])
+    delegateFunction name = ObjcMethod False "application" (SimpleType "void")
+      [ParamDef name (PointerType "UIApplication") "application"] []
+
+didLaunchFunction :: ObjcMethod
+didLaunchFunction = ObjcMethod False "app" (SimpleType "BOOL")
+  [ ParamDef "lication" (PointerType "UIApplication") "application"
+  , ParamDef "didFinishLaunchingWithOptions" (PointerType "NSDictionary") "launchOptions" ]
+  [ windowInit
+  , bgAssign
+  , vcInit
+  , rootVCAssignment
+  , windowAssignment
+  , ExpressionStatement windowMakeVisible
+  , ReturnStatement (BoolLit True) ]
+  where
+    mainScreen = MethodCall
+      (MethodCall (Var "UIScreen") (LibMethod "mainScreen" []) [])
+      (LibMethod "bounds" [])
+      []
+    windowAlloc = MethodCall (Var "UIWindow") (LibMethod "alloc" []) []
+    libInitWithFrame = LibMethod "initWith" ["Frame"]
+    windowInit = AssignStatement
+      (VarDecl (PointerType "UIWindow") "window")
+      (MethodCall windowAlloc libInitWithFrame [mainScreen])
+    bgAssign = AssignStatement
+      (PropertyCall (Var "window") "backgroundColor")
+      (MethodCall (Var "UIColor") (LibMethod "whiteColor" []) [])
+    vcAlloc = MethodCall (Var "ViewController") (LibMethod "alloc" []) []
+    vcInit = AssignStatement
+      (VarDecl (PointerType "ViewController") "mainViewController")
+      (MethodCall vcAlloc (LibMethod "init" []) [])
+    windowVar = Var "window"
+    selfWindowExpr = PropertyCall SelfExpr "window"
+    rootVCAssignment = AssignStatement
+      (PropertyCall windowVar "rootViewController")
+      (Var "mainViewController")
+    windowAssignment = AssignStatement selfWindowExpr windowVar
+    windowMakeVisible = MethodCall selfWindowExpr (LibMethod "makeKeyAndVisible" []) []
